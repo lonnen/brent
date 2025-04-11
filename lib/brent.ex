519424dfe62c141fc5f2bc160d5560fce1a2cc07
lonnen <chris.lonnen@gmail.com>
2025-04-11 01:11:24 -0700

    add a basic application

--- lib/brent.ex
+++ lib/brent.ex
@@ -1,18 +1,51 @@
+defmodule Brent.Application do
+  use Application
+
+  @impl true
+  def start(_type, _init_args) do
+    bot_options = %{
+      name: :goonwatch_bot,
+      consumer: Brent.Consumer,
+      intents: [:direct_messages, :guild_messages, :message_content],
+      wrapped_token: fn -> System.fetch_env!("BOT_TOKEN") end
+    }
+
+    children = [
+      {Nostrum.Bot, bot_options}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
+
 defmodule Brent do
-  @moduledoc """
-  Documentation for `Brent`.
-  """
+  use Nostrum.Consumer
 
-  @doc """
-  Hello world.
+  alias Nostrum.Api.Message
 
-  ## Examples
+  def start_link do
+    Consumer.start_link(__MODULE__)
+  end
 
-      iex> Brent.hello()
-      :world
+  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+    case msg.content do
+      "!goons" ->
+        Message.create(msg.channel_id, "Going to sleep...")
+        Process.sleep(3000)
+      
+      "!ping" ->
+        Message.create(msg.channel_id, "On point!")
 
-  """
-  def hello do
-    :world
+      "!raise" ->
+        # Remove later. This should not crash the whole bot
+        raise "I'm hit"
+
+      _ -> :ignore
+      end
+  end
+
+  # Ignore other events
+  def handle_event(_) do
+    :noop
   end
 end
