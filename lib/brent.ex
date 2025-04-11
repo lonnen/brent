ba1d004c65575358a5409291efaadc2adf4695aa
lonnen <chris.lonnen@gmail.com>
2025-04-10 23:58:06 -0700

    mix new
    Choosing Elixir because this project has a reasonably tight scope, I've wanted to try writing something in elixir, and messaging applications are supposed to be a natural fit.

---
+++ lib/brent.ex
@@ -0,0 +1,18 @@
+defmodule Brent do
+  @moduledoc """
+  Documentation for `Brent`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Brent.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/brent_test.exs
@@ -0,0 +1,8 @@
+defmodule BrentTest do
+  use ExUnit.Case
+  doctest Brent
+
+  test "greets the world" do
+    assert Brent.hello() == :world
+  end
+end
